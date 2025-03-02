c5d0f43a4f1b1b9d249f2536f906ddf20897a564
Vojtech Sliva <sli0124@vsb.cz>
2025-03-02 15:31:00 +0100

    feat(task1): implement Laplace smoothing for next word probability calculation

--- tasks/task1.py
+++ tasks/task1.py
@@ -29,19 +29,24 @@ def calculate_probabilities(n_grams_dict):
     return {k: v / total for k, v in n_grams_dict.items()}
 
 
-def calculate_next_word_probability(tokens, n):
+def calculate_next_word_probability_laplace(tokens, n, alpha=1):
+    # source: https://en.wikipedia.org/wiki/Additive_smoothing
     n_grams = create_n_grams_dict(tokens, n)
     n_plus_one_grams = create_n_grams_dict(tokens, n + 1)
 
+    vocab_size = len(set(word for line in tokens for word in line))
     next_word_probabilities = {}
+
     for n_plus_one_gram, count in n_plus_one_grams.items():
         n_gram = " ".join(n_plus_one_gram.split()[:-1])
         next_word = n_plus_one_gram.split()[-1]
-        if n_gram in n_grams:
-            probability = count / n_grams[n_gram]
-            if n_gram not in next_word_probabilities:
-                next_word_probabilities[n_gram] = {}
-            next_word_probabilities[n_gram][next_word] = probability
+
+        if n_gram not in next_word_probabilities:
+            next_word_probabilities[n_gram] = {}
+
+        probability = (count + alpha) / (n_grams.get(n_gram, 0) + alpha * vocab_size)
+        next_word_probabilities[n_gram][next_word] = probability
+
     return next_word_probabilities
 
 
@@ -51,20 +56,22 @@ def main():
 
     # task 1
     for n in range(1, 4):
-        print(f"Most frequent {n}-grams:")
+        print(f"\033[92mMost frequent {n}-grams:\033[0m")
         print(get_the_most_frequent_n_grams(tokens, n, 10), end="\n\n")
 
     # task 2
-    next_word_probabilities = calculate_next_word_probability(tokens, 2)
-    most_frequent_2_grams = get_the_most_frequent_n_grams(tokens, 2, 10)
-    for n_gram, _ in most_frequent_2_grams:
-        print(f"Next word probabilities for \"{n_gram}\":")
-        if n_gram in next_word_probabilities:
-            for next_word, probability in next_word_probabilities[n_gram].items():
-                print(f"{next_word}: {probability}")
-            print()
-        else:
-            print("No data available", end="\n\n")
+    for n in range(2, 4):
+        print(f"\033[92mMost frequent {n}-grams:\033[0m")
+        next_word_probabilities = calculate_next_word_probability_laplace(tokens, n)
+        most_frequent_2_grams = get_the_most_frequent_n_grams(tokens, n, 10)
+        for n_gram, _ in most_frequent_2_grams:
+            print(f"\033[94mNext word probabilities for \"{n_gram}\":\033[0m")
+            if n_gram in next_word_probabilities:
+                for next_word, probability in next_word_probabilities[n_gram].items():
+                    print(f"{next_word}: {probability}")
+                print()
+            else:
+                print("No data available", end="\n\n")
 
 
 if __name__ == "__main__":
