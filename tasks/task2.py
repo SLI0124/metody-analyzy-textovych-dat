b25f2a5a8fe03cd8d15f8c45ebf2dc4e2a2a4bc7
Vojtěch Slíva <sli0124@vsb.cz>
2025-03-04 12:55:17 +0100

    feat(task2): implement pattern searching algorithms

---
+++ tasks/task2.py
@@ -0,0 +1,136 @@
+def load_data(file_path):
+    with open(file_path, "r", encoding="utf-8") as file:
+        return file.read()
+
+
+def brute_force(data, match_string):
+    comparisons = 0
+    positions = []
+    for i in range(len(data) - len(match_string) + 1):
+        comparisons += 1
+        if data[i:i + len(match_string)] == match_string:
+            positions.append(i)
+    return comparisons, len(positions), positions
+
+
+def construct_kmp_table(pattern, lps):
+    length = 0
+    m = len(pattern)
+
+    lps[0] = 0
+
+    i = 1
+    while i < m:
+        if pattern[i] == pattern[length]:
+            length += 1
+            lps[i] = length
+            i += 1
+        else:
+            if length != 0:
+                length = lps[length - 1]
+            else:
+                lps[i] = 0
+                i += 1
+
+
+def kmp_search_algorithm(data, pattern):
+    """
+    https://www.geeksforgeeks.org/kmp-algorithm-for-pattern-searching/
+    """
+    n = len(data)
+    m = len(pattern)
+
+    lsp = [0] * m
+    result = []
+
+    construct_kmp_table(pattern, lsp)
+
+    i = 0
+    j = 0
+    comparisons = 0
+    positions = []
+
+    while i < n:
+        if data[i] == pattern[j]:
+            i += 1
+            j += 1
+            comparisons += 1
+
+            if j == m:
+                result.append(i - j)
+                positions.append(i - j)
+                j = lsp[j - 1]
+        else:
+            if j != 0:
+                j = lsp[j - 1]
+            else:
+                i += 1
+
+    return comparisons, len(positions), positions
+
+
+def bad_character_heuristic(pattern, size):
+    number_of_characters = 256
+    bad_characters = [-1] * number_of_characters
+    for i in range(size):
+        bad_characters[ord(pattern[i])] = i
+
+    return bad_characters
+
+
+def boyer_moore_search_algorithm(data, pattern):
+    """
+    https://www.geeksforgeeks.org/boyer-moore-algorithm-for-pattern-searching/
+    """
+    m = len(pattern)
+    n = len(data)
+
+    bad_characters = bad_character_heuristic(pattern, m)
+    shift = 0
+    comparisons = 0
+    positions = []
+    while shift <= n - m:
+        j = m - 1
+
+        while j >= 0 and pattern[j] == data[shift + j]:
+            j -= 1
+            comparisons += 1
+
+        if j < 0:
+            shift += (m - bad_characters[ord(data[shift + m])] if shift + m < n else 1)
+            positions.append(shift)
+        else:
+            shift += max(1, j - bad_characters[ord(data[shift + j])])
+
+    return comparisons, len(positions), positions
+
+
+def main():
+    data_files = {
+        "Short data": '../input/task2/short_sample.txt',
+        "Long data": '../input/task2/long_sample.txt',
+        "DNA data": '../input/task2/dna_sample.txt'
+    }
+
+    patterns = {
+        "Short data": ['little', 'he', 'legs'],
+        "Long data": ['little', 'he', 'legs'],
+        "DNA data": ['aagctt', 'cg', 'ccg']
+    }
+
+    algorithms = [brute_force, kmp_search_algorithm, boyer_moore_search_algorithm]
+
+    for data_name, file_path in data_files.items():
+        print(f"\n=== {data_name} ===")
+        data = load_data(file_path)
+
+        for pattern in patterns[data_name]:
+            print(f"\nPattern: '{pattern}'")
+            for algorithm in algorithms:
+                comparisons, count, positions = algorithm(data, pattern)
+                print(f"  - {algorithm.__name__:<25}\n\t-Comparisons: {comparisons:<6}\n\t-Count: {count:<4}"
+                      f"\n\t-Positions: {positions}")
+
+
+if __name__ == '__main__':
+    main()
